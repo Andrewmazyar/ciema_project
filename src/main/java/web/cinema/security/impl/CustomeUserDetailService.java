b2a5f3521bd44e37a49dfa32809a7e51d829c04c
Andrewmazyar <maziarandriy@gmail.com>
2020-06-17 16:56:33 +0300

    change maper in  customUserDetails

--- src/main/java/web/cinema/security/impl/CustomeUserDetailService.java
+++ src/main/java/web/cinema/security/impl/CustomeUserDetailService.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import web.cinema.model.Role;
 import web.cinema.model.User;
 import web.cinema.service.UserService;
 
