b828028d3386ae6892000362138e009c39b40b38
MAZIAR0ANDRIY <39693366+Andrewmazyar@users.noreply.github.com>
2020-06-24 21:46:25 +0300

    Clean up (#11)
    * clean up prject
    
    * clean up project
    
    * correct readme file
    
    * added shopping cart for admin
    
    * remove method remove from authentication Service

--- src/main/java/web/cinema/config/WebConfig.java
+++ src/main/java/web/cinema/config/WebConfig.java
@@ -1,11 +1,8 @@
 package web.cinema.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
@@ -13,13 +10,4 @@ import org.springframework.web.servlet.view.JstlView;
         "web.cinema.controllers"
 })
 public class WebConfig {
-
-    @Bean
-    public InternalResourceViewResolver getResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setViewClass(JstlView.class);
-        resolver.setPrefix("/WEB-INF/views/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
